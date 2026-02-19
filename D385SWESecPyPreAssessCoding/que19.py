d3615c0d4c72f3e24cfdd6c02ea620f6aa2f19ab
earl-sky <126019686+earl-sky@users.noreply.github.com>
2026-02-19 01:45:31 -0800

    add coding practice and create new flashcards from quizlet user

---
+++ D385SWESecPyPreAssessCoding/que19.py
@@ -0,0 +1,39 @@
+# You are being asked to validate that a numeric input is within a pre-defined range.
+
+# Using the provided template code, create an input validation check to ensure an input value is within the range of values from 1 to 10, inclusive. Use the range() function to check the input variable's value against the defined range. The code should output a message of "The number input is in the range from 1 and 10." or "The number input is not in the range from 1 and 10." depending on the numeric value of the variable.
+
+# For example, if the input within range is:
+
+# 8
+
+# The output to the console will be the following:
+
+# The number input is in the range from 1 and 10.
+
+# Alternatively, if the input out of range is:
+
+# 15
+
+# The output to the console will be the following:
+
+# The number input is not in the range from 1 and 10.
+
+#check if the input range is between 1 and 10 for the range validation check
+
+#   ____ ___  ____  ____  _____ ____ _____ 
+#  / ___/ _ \|  _ \|  _ \| ____/ ___|_   _|
+# | |  | | | | |_) | |_) |  _|| |     | |  
+# | |__| |_| |  _ <|  _ <| |__| |___  | |  
+#  \____\___/|_| \_\_| \_\_____\____| |_|  
+
+if __name__ == '__main__': 
+        
+    r = range(1,10)
+    
+    num = int(input())
+    
+    # create conditional statement for range check here
+    # Start my answer here
+    if num in r: print("The number input is in the range from 1 and 10.")
+    else: print("The number input is not in the range from 1 and 10.")
+    # End my answer here
